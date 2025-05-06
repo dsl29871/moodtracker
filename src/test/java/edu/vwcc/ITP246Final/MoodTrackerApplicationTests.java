cdf05febb60fc7fa21db930b5d6fdd2de94a9306
David Larkins <166552888+dsl29871@users.noreply.github.com>
2025-05-05 21:37:27 -0400

    Switch from H2 to PostGreSQL and remove h2

--- src/test/java/edu/vwcc/ITP246Final/MoodTrackerApplicationTests.java
+++ src/test/java/edu/vwcc/ITP246Final/MoodTrackerApplicationTests.java
@@ -2,12 +2,17 @@ package edu.vwcc.ITP246Final;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-class MoodTrackerApplicationTests {
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
+public class MoodTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
